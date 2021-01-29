a08c621855d2d4bf9cae470750758c2248b48e19
Antonio Gagliarducci <gagliarducci.antonio@posteo.net>
2021-01-30 00:21:40 +0100

    (WIP) First steps to parse formData content

--- main.go
+++ main.go
@@ -1,7 +1,54 @@
 package main
 
-import "net/http"
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
+)
+
+type User struct {
+	Nickname string
+	Password string
+}
+
+func login(w http.ResponseWriter, r *http.Request) {
+
+	if r.Method != "POST" {
+		http.Error(w, "Sorry, only POST method is supported.", http.StatusBadRequest)
+		return
+	}
+
+	// Call ParseForm() to parse the raw query and update r.PostForm and r.Form.
+	if err := r.ParseMultipartForm(0); err != nil {
+		fmt.Fprintf(w, "ParseMultipartForm() err: %v", err)
+		return
+	}
+
+	// create object user
+	user := new(User)
+
+	// fill the object with values
+	user.Nickname = r.FormValue("Username")
+	user.Password = r.FormValue("Password")
+
+	// convert it to json
+	b, err := json.Marshal(user)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	// return it
+	fmt.Fprintf(w, string(b))
+
+}
 
 func main() {
-	http.ListenAndServe(":8080", http.FileServer(http.Dir("public/")))
+	http.Handle("/", http.FileServer(http.Dir("public")))
+	http.HandleFunc("/api/users/login", login)
+
+	fmt.Printf("Starting server for testing HTTP POST...\n")
+	if err := http.ListenAndServe(":8080", nil); err != nil {
+		log.Fatal(err)
+	}
 }
